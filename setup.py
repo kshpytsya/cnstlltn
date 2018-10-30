04cd1ea82ed8f41c7fd5d6bc685c6f6f10fba228
Kyrylo Shpytsya <kshpitsa@gmail.com>
2018-10-30 14:02:46 +0200

    Rework dependency specs and add formic2 and braceexpand

--- setup.py
+++ setup.py
@@ -9,14 +9,16 @@ setup(
     package_dir={"": "src"},
     entry_points={"console_scripts": ["cnstlltn = cnstlltn._cli:main"]},
     install_requires=[
-        'ansimarkup~=1.4',
+        'ansimarkup>=1.4,<2',
         'atomicwrites>=1.2.1,<2',
         'attrdict>=2,<3',
-        'click~=7.0',
-        'filelock~=3.0.8',
-        'jinja2~=2.10',
+        'braceexpand>=0.1.2,<1',
+        'click>=7.0,<8',
+        'filelock>=3.0.8,<4',
+        'formic2>=1.0.3,<2',
+        'jinja2>=2.10,<3',
         'toposort>=1.5,<2',
-        'zope.interface~=4.5',
+        'zope.interface>=4.5,<5',
     ],
     extras_require={
         'aws': ['boto3~=1.9.5']
